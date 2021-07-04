3eac1c39e9a98d12b6edb0a349aac3ac83a012a1
Henry <henry@compree.com>
2021-07-05 01:12:52 +0200

    simple

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-docker run -it --rm godev bash
+#docker run -it --rm godev bash
+docker run -w /go/src -v /root/phixer/minimalrpc/:/go/src -v /root/phixer/minimalrpc/:/go/bin -it --rm godev bash
+
 
